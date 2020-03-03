2868d3da2d911543cf0bf9baadb3a31d182b6789
Everybody0523 <h.neukc@gmail.com>
2020-03-03 18:10:35 -0500

    liveness doesn't work but once we finish the framework it should

--- Assignment2-Dataflow_Analysis/src/liveness.cpp
+++ Assignment2-Dataflow_Analysis/src/liveness.cpp
@@ -4,7 +4,44 @@
 // @TODO 
 
 namespace {
-class Liveness final : public FunctionPass
+class Variable
+{
+private:
+        const Value * _val;
+public:
+        bool operator==(const Variable & other) const
+        {
+                // @TODO
+                return false;
+        }
+
+        const Value * getVal() const { return _val; }
+
+        //friend raw_ostream & operator<<(raw_ostream & outs, const Expression & expr);
+
+}; 
+}
+
+namespace std {
+
+// Construct a hash code for 'Expression'.
+template <>
+struct hash < Variable >
+{
+        std::size_t operator()(const Variable & var) const
+        {
+                std::hash < const Value * > pvalue_hasher;
+                std::size_t val_hash = pvalue_hasher((var.getVal()));
+
+                return (val_hash << 1) ^ (13);
+        }
+};
+}
+
+namespace {
+
+class Liveness final : public dfa::Framework < Variable,
+                                               dfa::Direction::Backward > 
 {
 public:
         static char ID;
@@ -14,17 +51,17 @@ public:
 
         // @TODO Add or remove method definitions if necessary.
 
-	
-
         virtual void getAnalysisUsage(AnalysisUsage &AU) const override
         {
                 AU.setPreservesAll();
         }
 
+        /*
         virtual bool runOnFunction(Function &func) override
         {
                 return false;
         }
+        */
 };
 
 char Liveness::ID = 1; 
