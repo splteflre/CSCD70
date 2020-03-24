cbd2a5f522a3471fb08b97f1be4be509e2ce94fb
Neumann <Neumann Hon>
2020-03-24 16:57:02 -0400

    This is not the proper way to merge

---
+++ Assignment2-Dataflow_Analysis/src/avail_expr.cpp
@@ -0,0 +1,179 @@
+#include "dfa/framework.h"
+
+namespace {
+
+class Expression
+{
+private:
+        unsigned _opcode; const Value * _lhs, * _rhs;
+public:
+        Expression(const Instruction & inst)
+        {
+                this->_opcode = inst.getOpcode();
+                this->_lhs = inst.getOperand(0);
+                this->_rhs = inst.getOperand(1);
+        }
+
+        bool operator==(const Expression & Expr) const
+        {
+                if (this->operationIsCommutative())
+                {
+                        bool isExactSame = (this->_opcode == Expr._opcode && this->_lhs == Expr._lhs && this->_rhs == Expr._rhs);
+                        bool switchedOperands = (this->getOpcode() == Expr.getOpcode() && this->getLHSOperand() == Expr.getRHSOperand() 
+                                                 && this->getRHSOperand() == Expr.getLHSOperand());
+                        return isExactSame || switchedOperands;
+                }
+                else
+                {
+                        return (this->_opcode == Expr._opcode && this->_lhs == Expr._lhs && this->_rhs == Expr._rhs);
+                }
+        }
+
+        unsigned getOpcode() const { return _opcode; }
+        const Value * getLHSOperand() const { return _lhs; }
+        const Value * getRHSOperand() const { return _rhs; }
+
+        bool operationIsCommutative() const;
+        
+        friend raw_ostream & operator<<(raw_ostream & outs, const Expression & expr);
+}; 
+
+raw_ostream & operator<<(raw_ostream & outs, const Expression & expr)
+{
+        outs << "[" << Instruction::getOpcodeName(expr._opcode) << " ";
+        expr._lhs->printAsOperand(outs, false); outs << ", ";
+        expr._rhs->printAsOperand(outs, false); outs << "]";
+
+        return outs;
+}
+
+bool Expression::operationIsCommutative() const
+{
+        bool out;
+        unsigned operation = this->getOpcode();
+        switch(operation)
+        {
+                case Instruction::Add:
+                case Instruction::FAdd:
+                case Instruction::Mul:
+                case Instruction::FMul:
+                case Instruction::And:
+                case Instruction::Or:
+                case Instruction::Xor:
+                        out = true;
+                        break;
+                default:
+                        out = false;
+                        break;
+        }
+        return out;
+}
+
+}  // namespace anonymous
+
+namespace std {
+
+// Construct a hash code for 'Expression'.
+template <>
+struct hash < Expression >
+{
+        std::size_t operator()(const Expression & expr) const
+        {
+                std::hash < unsigned > unsigned_hasher; std::hash < const Value * > pvalue_hasher;
+
+                std::size_t opcode_hash = unsigned_hasher(expr.getOpcode());
+                std::size_t lhs_operand_hash = pvalue_hasher((expr.getLHSOperand()));
+                std::size_t rhs_operand_hash = pvalue_hasher((expr.getRHSOperand()));
+
+                return opcode_hash ^ (lhs_operand_hash << 1) ^ (rhs_operand_hash << 1);
+        }
+};
+
+}  // namespace std
+
+namespace {
+
+class AvailExpr final : public dfa::Framework < Expression, 
+                                                dfa::Direction::Forward >
+{
+protected:
+        virtual BitVector IC() const override
+        {
+                return BitVector(_domain.size(), true);
+        }
+        virtual BitVector BC() const override
+        {
+                return BitVector(_domain.size());
+        }
+        virtual BitVector MeetOp(const meetop_const_range & meet_operands) const override
+        {
+                // @TODO
+                
+                BitVector ret = BitVector(_domain.size(), true);
+
+                for(auto pred : meet_operands) {
+                        Instruction* last_inst = LLVMGetLastInstruction(pred);
+                        auto last_bv = _inst_bv_map.at(last_inst);
+                        ret &= last_bv;
+                }
+                return ret;
+                // return BitVector(_domain.size());
+        }
+        virtual bool TransferFunc(const Instruction & inst,
+                                  const BitVector & ibv,
+                                  BitVector & obv) override
+        {
+                BitVector bv_prime = ibv;
+                //const Value inst_v = inst;
+
+                for (auto prev_exp : _domain){
+                    if (prev_exp.getLHSOperand() == &(cast<Value>(inst)) || prev_exp.getRHSOperand() == &(cast<Value>(inst))){
+                        int idx = std::distance(_domain.begin(), _domain.find(inst));
+                        bv_prime.reset(idx); 
+                    }
+                }
+                Expression cur_exp = Expression(inst);
+                auto found = _domain.find(cur_exp);
+                if(found != _domain.end()){
+                    int idx = std::distance(_domain.begin(), found);
+                    bv_prime.set(idx);
+                }
+                bool hasChange = !(obv == bv_prime);
+
+                /*
+                outs() << "================================================================\n"; 
+                outs() << "inst: " << inst << "\n";
+                outs() << "input bit vector\n";
+                printDomainWithMask(ibv);
+                outs() << "\n";
+                outs() << "output bit vector\n";
+                printDomainWithMask(bv_prime);
+                outs() << "\n";
+                outs() << "================================================================\n"; 
+                */
+                obv = bv_prime;
+                return hasChange;
+        }
+        virtual void InitializeDomainFromInstruction(const Instruction & inst) override
+        {
+                // try to insert the instruction into the domain, and throw an
+                // `invalid_argument` exception if failed
+                try {
+                        if (isa<BinaryOperator>(inst))
+                        {
+                                _domain.emplace(inst);
+                        }
+                } catch (const std::invalid_argument & ia_except) {}
+        }
+public:
+        static char ID;
+
+        AvailExpr() : dfa::Framework < domain_element_t, 
+                                       direction_c > (ID) {}
+        virtual ~AvailExpr() override {}
+};
+
+char AvailExpr::ID = 1; 
+RegisterPass < AvailExpr > Y ("avail_expr", "Available Expression");
+
+} // namespace anonymous
---
+++ Assignment2-Dataflow_Analysis/src/liveness.cpp
@@ -0,0 +1,200 @@
+#include "dfa/framework.h"
+#include <unordered_set>
+
+// @TODO 
+
+namespace {
+class Variable
+{
+private:
+        const Value * _val;
+public:
+        Variable(const Value & val):
+        _val(&val)
+        {
+        }
+
+        bool operator==(const Variable & other) const
+        {
+                return (this->getVal() == other.getVal());
+        }
+
+        const Value * getVal() const { return _val; }
+
+
+        friend raw_ostream & operator<<(raw_ostream & outs, const Variable & var);
+}; 
+
+raw_ostream & operator<<(raw_ostream & outs, const Variable & var)
+{
+        outs << "[";
+        var.getVal()->printAsOperand(outs, false);
+        outs << "]";
+        return outs;
+}
+
+}
+
+namespace std {
+
+// Construct a hash code for 'Expression'.
+template <>
+struct hash < Variable >
+{
+        std::size_t operator()(const Variable & var) const
+        {
+                std::hash < const Value * > pvalue_hasher;
+                std::size_t val_hash = pvalue_hasher((var.getVal()));
+
+                return (val_hash << 1) ^ (1934773);
+        }
+};
+}
+
+namespace {
+
+class Liveness final : public dfa::Framework < Variable,
+                                               dfa::Direction::Backward > 
+{
+public:
+        static char ID;
+
+        Liveness() : dfa::Framework<domain_element_t, direction_c>(ID) {}
+        virtual ~Liveness() override {}
+
+        virtual void getAnalysisUsage(AnalysisUsage &AU) const override
+        {
+                AU.setPreservesAll();
+        }
+
+        virtual BitVector IC() const override
+        {
+                return BitVector(_domain.size(), false);
+        }
+        virtual BitVector BC() const override
+        {
+                return BitVector(_domain.size(), false);
+        }
+        virtual BitVector MeetOp(const meetop_const_range & meet_operands) const override
+        {
+                //return BitVector(_domain.size());
+                BitVector ret = BitVector(_domain.size(), false); 
+                
+                for (auto succ : meet_operands) {
+                        Instruction* first_inst = LLVMGetFirstInstruction(succ);
+                        auto first_bv = _inst_bv_map.at(first_inst);
+                        ret |= first_bv;
+                }
+                return ret;
+        }
+        virtual bool TransferFunc(const Instruction & inst,
+                                  const BitVector & ibv,
+                                  BitVector & obv) override
+        {
+                const BitVector old_obv = obv;
+                BitVector use_s = BitVector(_domain.size(), false);
+                BitVector def_s = BitVector(_domain.size(), false);
+                BitVector bv_prime = ibv; 
+
+                // Compute use_s
+                for (auto iter = inst.op_begin(); iter != inst.op_end(); ++iter)
+                {
+                        Value * val = *iter;
+                        
+                        if (isa<Instruction>(val) || isa<Argument>(val))
+                        {
+                                Variable curr_var = Variable(*val);
+                                auto found = _domain.find(curr_var);
+                                if (found != _domain.end())
+                                {
+                                        int idx = std::distance(_domain.begin(), found);
+                                        use_s.set(idx);
+                                }
+                        }
+                }
+
+                // Compute def_s, but only if we actually
+                // have a "lvalue" for this instruction.
+                // Instructions that don't return (eg: branches)
+                // obviously don't define anything.
+                if (!(inst.getType()->isVoidTy()))
+                {
+                        Variable curr_var = Variable(inst);
+                        auto found = _domain.find(curr_var);
+                        if (found != _domain.end())
+                        {
+                                int idx = std::distance(_domain.begin(), found);
+                                def_s.set(idx);
+                        }
+                }
+
+                // Compute x - def_s
+                def_s = def_s.flip();
+                bv_prime &= def_s;
+                use_s |= bv_prime;
+                obv = use_s;
+
+                auto parentBB = inst.getParent();
+                if (&inst == LLVMGetLastInstruction(parentBB))
+                {
+                        for (const auto bb_succ : successors(parentBB))
+                        {
+                                // Walk all phi nodes 
+                                const auto phiInstructions = bb_succ->phis();
+                                for (auto phiIter = phiInstructions.begin(); phiIter != phiInstructions.end(); ++phiIter)
+                                {
+                                        const PHINode * phiInst = &(*phiIter);
+                                        unsigned numIncVal = phiInst->getNumIncomingValues();
+                                        for (unsigned i = 0; i < numIncVal; ++i)
+                                        {
+                                                if (phiInst->getIncomingBlock(i) != parentBB)
+                                                {
+                                                        // Remove from set if value comes from different basic block.
+                                                        Value * otherBlockVal = phiInst->getIncomingValue(i);
+                                                        Variable curr_var = Variable(*otherBlockVal);                                
+                                                        auto found = _domain.find(curr_var);
+                                                        if (found != _domain.end())
+                                                        {
+                                                                int idx = std::distance(_domain.begin(), found);
+                                                                obv.reset(idx);
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                }
+                return (obv != old_obv);
+        }
+        virtual void InitializeDomainFromInstruction(const Instruction & inst) override
+        {
+                // Add each operand
+                for (auto iter = inst.op_begin(); iter != inst.op_end(); ++iter)
+                {
+                        Value * val = *iter;
+                        if (isa<Instruction>(val) || isa<Argument>(val))
+                        {
+                                try
+                                {
+                                        _domain.emplace(*val);
+                                }
+                                catch (const std::invalid_argument & ia_except) {}        
+                        }
+                }
+
+                // If inst has a "lvalue", add it to the domain too.
+                if (!(inst.getType()->isVoidTy()))
+                {
+                        try
+                        {
+                                _domain.emplace(inst);
+                        }
+                        catch (const std::invalid_argument & ia_except) {}
+                }
+        }
+protected:
+};
+
+char Liveness::ID = 1; 
+RegisterPass < Liveness > Y ("liveness", "Liveness");
+
+} // namespace anonymous
